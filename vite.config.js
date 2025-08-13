4ab328147565d19a369266b4dbb432c5d6ef68a6
hardikjha <kumarhardik436@gmail.com>
2025-08-13 11:50:51 +0530

    added comments

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true // Dev server fix for React Router
+    historyApiFallback: true 
   },
   build: {
     rollupOptions: {},
